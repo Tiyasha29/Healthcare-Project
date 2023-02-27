d98c579ab107c0f62acc5e90b26d091e4d2d547c
Tiyasha29 <79760452+Tiyasha29@users.noreply.github.com>
2023-02-27 12:52:39 +0530

    Update About.js

--- About.js
+++ About.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import aboutimage from "../assets/aboutpageimage.jpg";
 
 export default function About() {
+  const myAboutStyle={"font-family": "Inter",
+  "padding": "20px",
+  "align": "left",
+  "display": "flex"}
   return (
-    <div><h3>About</h3><p>This paragraph talks about our website in details.</p></div>
+    <>
+    <div style={myAboutStyle}>
+      <h3 style={{"padding":"20px", "font-weight":"bold"}}>About Our Platform</h3>
+      <p style={{"padding":"20px"}}>Our smart healthcare platform seeks to 
+    provide a one stop destination for patients to connect with healthcare providers around them, log in their 
+    symptoms if any as well as securely store and monitor medical reports daily. The thrust of the mission 
+    is on establishing a fully functional, centralised and standard platform to provide the most seamless 
+    experience to patients around the world. Health and well-being of people form the centre of any nation’s 
+    public policy. The rate of ill-health affects economic prospects, lowers the capacity to accumulate health 
+    capital, quality of life and labour market outcomes. The COVID-19 pandemic has further exposed the 
+    stresses and weaknesses of the world’s healthcare system. As a 
+    patient, being frustrated with care that is inflexible, impersonal and inefficient is natural and these 
+    individual experiences add up to poor safety and care coordination that may cost lives.
+      </p>
+      <img src={aboutimage} alt="demonstrative" style={{"width": "800px", "height": "500px", "padding":"20px"}}/>
+    </div>
+    <p style={{"padding":"20px","font-family":"Inter","font-weight":"bold","text-align":"center","border-color":"lightblue", "border-style":"outset", "border-radius":"5px", "box-shadow":"10px 10px 5px lightblue"}}>A journey to better healthcare.</p>
+    </>
   )
 }
