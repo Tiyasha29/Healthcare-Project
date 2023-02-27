9cb9a8987e71e72e7c8578fe89c0e186f1287c65
Tiyasha29 <79760452+Tiyasha29@users.noreply.github.com>
2023-02-27 12:50:43 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,21 +2,30 @@ import './App.css';
 import './MyComponents/Header.js';
 import Header from './MyComponents/Header';
 import About from './MyComponents/About';
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Footer from './MyComponents/Footer';
 import Slideshow from './MyComponents/Slideshow';
-import TopSection from './MyComponents/TopSection';
+import React, { useState,useEffect } from 'react';
+import DisplayPrediction from './MyComponents/DisplayPrediction';
 
 function App() {
+  
+  React.useEffect(()=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+      console.log(position.coords)
+    })
+  },[])
+
   return (
   <>
   <Header/>
   <Router>
     <Routes>
-    <Route exact path="/" element={<Slideshow/>}></Route>
+    <Route exact path="/" element={<><Slideshow/><DisplayPrediction/></>}></Route>
       <Route exact path="/about" element={<About/>}></Route>           
     </Routes>
   </Router>
+  
   <Footer/>
   </>
   );
