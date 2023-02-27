4a399a5423cff1a909f5efc994cf21f680f11b2a
Tiyasha29 <79760452+Tiyasha29@users.noreply.github.com>
2023-02-27 12:54:29 +0530

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     const myStyle={width:"100%",
-    top:"100vh",
+    top:"300vh",
     position:"absolute"};
   return (
     <footer className='bg bg-dark text-white text-center py-3' style={myStyle}>Copyright @smarthealthcare.com</footer>
